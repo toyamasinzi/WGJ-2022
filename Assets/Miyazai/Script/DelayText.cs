15c2430ae70d59b1da0be92d8caf6474a46126cc
Hikanyan <hikanyan0131@gmail.com>
2022-12-22 13:28:49 +0900

    DelayText完成

--- Assets/Miyazai/Script/DelayText.cs
+++ Assets/Miyazai/Script/DelayText.cs
@@ -3,31 +3,39 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Windows;
 
 public class DelayText : MonoBehaviour
 {
 
-    [SerializeField,Tooltip("会話のテキストを入力")]
-    string _input;
+    [SerializeField, Tooltip("会話のテキストを入力")]
+    List<string> _input;
     string _output;
-    [SerializeField]
+    [SerializeField,Tooltip("テキストUIを入れる")]
     Text _textUI;
-    [SerializeField] 
+    [SerializeField, Tooltip("1文字ずつ表示する時間(ミリ秒)")]
     int _waitTimeChar;
+    [SerializeField,Tooltip("次の会話までの待ち時間(ミリ秒)")]
+    int _waitTimeLine;
 
     private async void Start()
     {
-        await OutputMessage(_input);
+        foreach (var input in _input)
+        {
+            await OutputMessage(input);
+            await Task.Delay(_waitTimeLine);
+        }
     }
     //1文字ずつ表示する
     async Task OutputMessage(string s)
     {
-        foreach(char c in s)
+        foreach (char c in s)
         {
             _output += c.ToString();
             _textUI.text = _output;
             await Task.Delay(_waitTimeChar);
         }
+        _output += $"\n";
+        _textUI.text = _output;
     }
 }
+
