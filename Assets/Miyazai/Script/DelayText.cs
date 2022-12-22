3c746c667217d6684c024a9889e7dfb19646888d
Hikanyan <hikanyan0131@gmail.com>
2022-12-22 10:50:47 +0900

    DelayText

--- Assets/Miyazai/Script/DelayText.cs
+++ Assets/Miyazai/Script/DelayText.cs
@@ -1,8 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class DelayText : MonoBehaviour
 {
-    //async void 
+
+    [SerializeField,Tooltip("会話のテキストを入力")]
+    string _output;
+    [SerializeField]
+    Text _textUI;
+    [SerializeField] 
+    int _waitTimeChar;
+    //1文字ずつ表示する
+    async Task OutputMessage(string s)
+    {
+        foreach(char c in s)
+        {
+            _output += c.ToString();
+            _textUI.text = _output;
+            await Task.Delay(_waitTimeChar);
+        }
+    }
 }
